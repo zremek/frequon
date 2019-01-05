1b6e046896aebc33efed50f56fba2899fa99c4ba
Remigiusz <remigiuszzulicki@gmail.com>
2019-01-05 19:21:57 +0100

    add script

---
+++ script.R
@@ -0,0 +1,9 @@
+library(BetaBit)
+frequon()
+help(frequon)
+(lyo <- BetaBit::lyo)
+(pcs <- BetaBit::pcs)
+(pistoale <- BetaBit::pistoale)
+(roses <- BetaBit::roses)
+
+frequon(subject = "Re: Interested?", content = "")
